527e0981de03023b9826203438f6bc1f0eb68b65
kelvinhu9988 <kelvinhu9988@gmail.com>
2016-12-01 21:44:49 +0800

    changed graphics

---
+++ src/Bonus.cpp
@@ -0,0 +1,9 @@
+//
+//  Bonus.cpp
+//  Spaceship Battle Game
+//
+//  Created by Kelvin Hu on 12/1/16.
+//  Copyright © 2016 Kelvin Hu. All rights reserved.
+//
+
+#include "Bonus.hpp"
---
+++ src/Bonus.hpp
@@ -0,0 +1,14 @@
+//
+//  Bonus.hpp
+//  Spaceship Battle Game
+//
+//  Created by Kelvin Hu on 12/1/16.
+//  Copyright © 2016 Kelvin Hu. All rights reserved.
+//
+
+#ifndef Bonus_hpp
+#define Bonus_hpp
+
+#include <stdio.h>
+
+#endif /* Bonus_hpp */
